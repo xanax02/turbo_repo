e6c2c03fe87a68448e480bfb1091c7bbc94251f9
abhay <abhaytxanax@gmail.com>
2024-04-07 23:36:46 +0530

    esbuild for backend

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,7 +1,9 @@
 import express from "express";
+import { BACKEND_URL } from "@repo/common/config";
 
 const app = express();
 
+console.log(BACKEND_URL)
 app.get("/", (req, res) => {
     res.json({
         message: "Hello World"
